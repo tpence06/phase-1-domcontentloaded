271f37b07a6d9dfad7067b685bc00a6057c9ec25
tpence06 <tpence06@hotmail.com>
2023-02-23 17:19:36 -0700

    pass

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(p) {
+    console.log("The DOM has loaded");
+
+const paragraph = document.querySelector("p")
+const change = document.createElement("p");
+change.innerHTML = 'This is really cool!';
+paragraph.parentNode.replaceChild(change, paragraph);
+  });
